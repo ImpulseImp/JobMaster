41075b9954160eb059319109bf48a900924ffd1d
Impulse <impulses4mini@gmail.com>
2025-03-18 00:01:33 +0300

    remove unused imports

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -5,12 +5,6 @@ import { Moon, Sun, MonitorCog } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 function ModeToggle() {
   const { setTheme } = useTheme();
