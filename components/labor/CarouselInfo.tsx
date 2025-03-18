58bae8915a63b5f7f898d544a53a2df372872dd3
Impulse <impulses4mini@gmail.com>
2025-03-18 15:24:12 +0300

    fix carousel width

--- components/labor/CarouselInfo.tsx
+++ components/labor/CarouselInfo.tsx
@@ -10,14 +10,14 @@ import {
 function CarouselInfo() {
   return (
     <div className='relative'>
-      <Carousel className='w-full max-w-xl '>
+      <Carousel className='w-full max-w-sm md:max-w-xl '>
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index}>
               <div className='p-1 '>
                 <Card>
                   <CardContent className='flex aspect-square items-center justify-center p-6'>
-                    <span className='text-4xl font-semibold'>
+                    <span className='text-4xl text-center font-semibold'>
                       Последние новости {index + 1}
                     </span>
                   </CardContent>
