337a6145c78e9ca0c544b6db59e37f9579b688a4
Impulse <impulses4mini@gmail.com>
2025-03-18 21:01:23 +0300

    add auto play

--- components/labor/CarouselInfo.tsx
+++ components/labor/CarouselInfo.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
@@ -6,14 +7,22 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { useRef } from 'react';
 
 function CarouselInfo() {
+  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: true }));
   return (
     <div className='relative '>
-      <Carousel className='w-full  max-w-sm md:max-w-xl lg:max-w-3xl  '>
+      <Carousel
+        className='w-full max-w-sm md:max-w-xl lg:max-w-2xl'
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
+            <CarouselItem key={index} className=''>
               <div className='p-1 '>
                 <Card>
                   <CardContent className='flex aspect-square items-center justify-center p-6'>
--- components/labor/LaborContent.tsx
+++ components/labor/LaborContent.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function LaborContent() {
   return (
-    <div className='flex flex-col '>
+    <div className='flex flex-col max-w-3xl space-y-4'>
       {/* Item 1 */}
       <div className='p-2 border-2 border-gray-500 w-full space-y-2'>
         <div className='bg-red-200  '>
@@ -16,6 +16,104 @@ function LaborContent() {
           nam illo magni. Tempore repellat labore, placeat repellendus
           praesentium magnam nobis est reiciendis facilis? Dicta quidem odit
           natus recusandae veniam!
+        </p>
+      </div>
+      {/* Item 2 */}
+      <div className='p-2 border-2 border-gray-500 w-full space-y-2'>
+        <div className='bg-red-200  '>
+          <Button asChild>
+            <Link href={'/'}>Проверка знаний</Link>
+          </Button>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe velit
+          nam illo magni. Tempore repellat labore, placeat repellendus
+          praesentium magnam nobis est reiciendis facilis? Dicta quidem odit
+          natus recusandae veniam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ipsam
+          dolores odit sunt ipsum beatae omnis! Cum ipsum neque eaque tempora,
+          corporis nemo magnam non distinctio, nulla, illo ratione enim. Modi
+          adipisci recusandae id et ullam aliquam sunt natus quia dolore sit quo
+          repellat nisi soluta porro consequatur excepturi dolor accusantium
+          saepe eligendi at quae facilis, ratione rerum praesentium. Eveniet.
+          Delectus odit unde eius corrupti dolorum ducimus, eveniet assumenda
+          iusto, voluptatum in reprehenderit natus perspiciatis beatae animi
+          nesciunt reiciendis quidem quae. Soluta, tenetur doloribus temporibus
+          et nam dolorem molestiae ducimus. Nulla quod quis ex dicta accusamus!
+          Culpa nesciunt magnam fugiat et. Sed, vero officiis dolorem excepturi
+          obcaecati enim minima quia, mollitia quod culpa consequuntur, dolore
+          atque esse. Enim, iure minima? Architecto debitis magnam tenetur at
+          libero suscipit praesentium obcaecati? Cum ipsam, aut aperiam nisi
+          expedita unde temporibus. Assumenda, ullam, illum beatae placeat,
+          soluta obcaecati tempora omnis aut fuga accusamus consectetur?
+          Temporibus sapiente aperiam, in officia sit amet voluptates quod
+          tenetur libero ea, maiores, fugit nemo est corporis non? Reiciendis
+          fuga neque libero voluptatem doloribus earum dolor aliquam harum
+          labore nisi? Provident culpa, nisi vitae dolorum consequatur quidem
+          illo, et voluptas qui, totam numquam porro quasi. Voluptate,
+          repellendus eos deleniti voluptatem eius delectus assumenda harum!
+          Saepe ipsa repudiandae debitis molestias veniam. Amet, laudantium
+          modi. Iusto cupiditate impedit aut numquam sunt autem obcaecati
+          suscipit id fugit! Provident fuga libero numquam, earum iure error qui
+          impedit itaque quo ratione exercitationem, accusantium repellat
+          obcaecati. Fugiat, fuga repellat molestias nihil vel cumque! Ratione
+          nisi molestias minus consequuntur, modi expedita perferendis, quasi
+          culpa iste ut facere delectus quisquam maiores itaque! Qui veritatis
+          veniam corrupti totam dignissimos? Quaerat vel dolorum eos aperiam ea
+          perspiciatis fugit fuga quidem adipisci velit, cum voluptate
+          reiciendis totam rem, blanditiis exercitationem eius inventore
+          praesentium! Sit, qui doloribus ipsa suscipit cum possimus adipisci.
+        </p>
+      </div>
+      {/* Item 3 */}
+      <div className='p-2 border-2 border-gray-500 w-full space-y-2'>
+        <div className='bg-red-200  '>
+          <Button asChild>
+            <Link href={'/'}>Проверка знаний</Link>
+          </Button>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe velit
+          nam illo magni. Tempore repellat labore, placeat repellendus
+          praesentium magnam nobis est reiciendis facilis? Dicta quidem odit
+          natus recusandae veniam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ipsam
+          dolores odit sunt ipsum beatae omnis! Cum ipsum neque eaque tempora,
+          corporis nemo magnam non distinctio, nulla, illo ratione enim. Modi
+          adipisci recusandae id et ullam aliquam sunt natus quia dolore sit quo
+          repellat nisi soluta porro consequatur excepturi dolor accusantium
+          saepe eligendi at quae facilis, ratione rerum praesentium. Eveniet.
+          Delectus odit unde eius corrupti dolorum ducimus, eveniet assumenda
+          iusto, voluptatum in reprehenderit natus perspiciatis beatae animi
+          nesciunt reiciendis quidem quae. Soluta, tenetur doloribus temporibus
+          et nam dolorem molestiae ducimus. Nulla quod quis ex dicta accusamus!
+          Culpa nesciunt magnam fugiat et. Sed, vero officiis dolorem excepturi
+          obcaecati enim minima quia, mollitia quod culpa consequuntur, dolore
+          atque esse. Enim, iure minima? Architecto debitis magnam tenetur at
+          libero suscipit praesentium obcaecati? Cum ipsam, aut aperiam nisi
+          expedita unde temporibus. Assumenda, ullam, illum beatae placeat,
+          soluta obcaecati tempora omnis aut fuga accusamus consectetur?
+          Temporibus sapiente aperiam, in officia sit amet voluptates quod
+          tenetur libero ea, maiores, fugit nemo est corporis non? Reiciendis
+          fuga neque libero voluptatem doloribus earum dolor aliquam harum
+          labore nisi? Provident culpa, nisi vitae dolorum consequatur quidem
+          illo, et voluptas qui, totam numquam porro quasi. Voluptate,
+          repellendus eos deleniti voluptatem eius delectus assumenda harum!
+          Saepe ipsa repudiandae debitis molestias veniam. Amet, laudantium
+          modi. Iusto cupiditate impedit aut numquam sunt autem obcaecati
+          suscipit id fugit! Provident fuga libero numquam, earum iure error qui
+          impedit itaque quo ratione exercitationem, accusantium repellat
+          obcaecati. Fugiat, fuga repellat molestias nihil vel cumque! Ratione
+          nisi molestias minus consequuntur, modi expedita perferendis, quasi
+          culpa iste ut facere delectus quisquam maiores itaque! Qui veritatis
+          veniam corrupti totam dignissimos? Quaerat vel dolorum eos aperiam ea
+          perspiciatis fugit fuga quidem adipisci velit, cum voluptate
+          reiciendis totam rem, blanditiis exercitationem eius inventore
+          praesentium! Sit, qui doloribus ipsa suscipit cum possimus adipisci.
         </p>
       </div>
     </div>
