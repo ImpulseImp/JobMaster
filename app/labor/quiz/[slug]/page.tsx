ac73fa204667a28c816cbdc0bbb5dc16f2a09cae
Impulse <impulses4mini@gmail.com>
2025-04-02 16:00:48 +0300

    remove quizpage

--- app/labor/quiz/[slug]/page.tsx
+++ app/labor/quiz/[slug]/page.tsx
@@ -1,28 +1,4 @@
-import prisma from '@/utils/db';
-
-export default async function QuizPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } = params;
-
-  // Fetch quiz data on the server
-  const quiz = await prisma.quiz.findFirst({
-    where: { slug },
-    include: {
-      questions: {
-        include: {
-          options: true,
-        },
-      },
-    },
-  });
-
-  if (!quiz) {
-    return <p className='text-center mt-6'>Экзамен не найден.</p>;
-  }
-
-  // Pass the quiz data to the client component
-  return null;
+function QuizPage() {
+  return <div>QuizPage</div>;
 }
+export default QuizPage;
