58afd7df594d7fc21ff7089b5967a330ae23d79b
Impulse <impulses4mini@gmail.com>
2025-04-02 15:48:26 +0300

    reset quiz

--- app/labor/quiz/page.tsx
+++ app/labor/quiz/page.tsx
@@ -1,22 +1,4 @@
-import QuizContainer from '@/components/quiz/QuizContainer';
-import { fetchQuizCategories } from '@/utils/actions';
-
-async function QuizPage() {
-  const quizCategories = await fetchQuizCategories();
-
-  if (!quizCategories || quizCategories.length === 0) {
-    return <p>No quiz categories available.</p>;
-  }
-
-  return (
-    <div className=''>
-      <h1 className='text-center text-3xl'>
-        Добро пожаловать на страницу Экзамена
-      </h1>
-      <div>
-        <QuizContainer quizCategories={quizCategories} />
-      </div>
-    </div>
-  );
+function QuizPage() {
+  return <div>QuizPage</div>;
 }
 export default QuizPage;
